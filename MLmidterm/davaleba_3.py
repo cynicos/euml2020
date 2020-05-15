e7b409a21ff1c4a3b64bbfb363d65c00501ba1bd
cynicos <9davita9@gmail.com>
2020-05-15 14:25:50 +0400

    midterm

--- MLmidterm/davaleba_3.py
+++ MLmidterm/davaleba_3.py
@@ -7,22 +7,16 @@ Created on Fri May 15 11:29:41 2020
 
 
 import random
-# =============================================================================
-# M=int(input("შეიყვანეთ M: "))
-# N=int(input("შეიყვანეთ N: "))
-# =============================================================================
+M=int(input("შეიყვანეთ M: "))
+N=int(input("შეიყვანეთ N: "))
 
-# =============================================================================
+Matrix = [[0 for x in range(M)] for y in range(N)] 
 
-# 
-# i = random.randrange(0, 10)
-# print(i)
-# Matrix = [[random.randrange(0, 10) for x in range(3)] for y in range(4)] 
-# print(Matrix)
-# 
-# for j in range(3):
-#     for i in range(4):
-#         Matrix[i][j]
-#         print (Matrix[i][j])
-# =============================================================================
-
+for i in range(len(Matrix)) :  
+    for j in range(len(Matrix[i])) : 
+        if(i==0):
+            Matrix[i][j]=random.randrange(1, 10)
+        else:
+            Matrix[i][j]=Matrix[i-1][j]*2
+        print(Matrix[i][j], end=" ") 
+    print()  
